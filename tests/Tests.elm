167a159cd1c669010491d93920897f6e1c137f01
imtaplet <rockgajoen@gmail.com>
2020-06-27 16:06:02 +0900

    reduce calling amount functions.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -13,31 +13,31 @@ all =
                 \_ ->
                     let
                         amount =
-                            Dollar 5 |> Money.times 2 |> Money.amount
+                            Dollar 5 |> Money.times 2
                     in
-                    amount |> Expect.equal 10
+                    amount |> Expect.equal (Dollar 10)
             , test "Multiplication 2" <|
                 \_ ->
                     let
                         fifteen =
-                            Dollar 5 |> Money.times 3 |> Money.amount
+                            Dollar 5 |> Money.times 3
                     in
-                    fifteen |> Expect.equal 15
+                    fifteen |> Expect.equal (Dollar 15)
             ]
         , describe "Franc"
             [ test "Multiplication 1" <|
                 \_ ->
                     let
                         amount =
-                            Franc 5 |> Money.times 2 |> Money.amount
+                            Franc 5 |> Money.times 2
                     in
-                    amount |> Expect.equal 10
+                    amount |> Expect.equal (Franc 10)
             , test "Multiplication 2" <|
                 \_ ->
                     let
                         fifteen =
-                            Franc 5 |> Money.times 3 |> Money.amount
+                            Franc 5 |> Money.times 3
                     in
-                    fifteen |> Expect.equal 15
+                    fifteen |> Expect.equal (Franc 15)
             ]
         ]
