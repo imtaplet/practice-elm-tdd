79e85415fb72321e30cb59fa77d58df716e1f572
imtaplet <rockgajoen@gmail.com>
2020-06-27 13:31:14 +0900

    format

--- src/Dollar.elm
+++ src/Dollar.elm
@@ -1,13 +1,18 @@
 module Dollar exposing (..)
 
-type alias Amount = Int
 
-type Dollar 
+type alias Amount =
+    Int
+
+
+type Dollar
     = Dollar Int
+
 
 times : Int -> Dollar -> Dollar
 times multiplier (Dollar dollar) =
-    Dollar <| multiplier * dollar 
+    Dollar <| multiplier * dollar
+
 
 amount : Dollar -> Int
 amount (Dollar dollar) =
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,26 +1,27 @@
 module Tests exposing (..)
 
-import Test exposing (..)
-import Expect
-
 import Dollar exposing (..)
+import Expect
+import Test exposing (..)
+
 
 all : Test
 all =
     describe "Money Test"
         [ describe "Dollar"
             [ test "Multiplication 1" <|
-                \_ -> 
+                \_ ->
                     let
-                        amount = Dollar 5 |> Dollar.times 2 |> Dollar.amount
+                        amount =
+                            Dollar 5 |> Dollar.times 2 |> Dollar.amount
                     in
-                        amount |> Expect.equal 10
+                    amount |> Expect.equal 10
             , test "Multiplication 2" <|
                 \_ ->
                     let
                         fifteen =
                             Dollar 5 |> Dollar.times 3 |> Dollar.amount
                     in
-                        fifteen |> Expect.equal 15
+                    fifteen |> Expect.equal 15
             ]
         ]
